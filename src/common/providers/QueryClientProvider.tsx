93ab685cf5da255c64271b018cbc4e19acc84feb
ahmad_chand <theahmadchand@gmail.com>
2024-04-07 02:56:53 +0500

    feat: remove react-query devTools

--- src/common/providers/QueryClientProvider.tsx
+++ src/common/providers/QueryClientProvider.tsx
@@ -1,14 +1,8 @@
 import { QueryClient, QueryClientProvider as TanStackQueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const QueryClientProvider: FCC = (props) => {
     const { children } = props;
     const queryClient = new QueryClient();
 
-    return (
-        <TanStackQueryClientProvider client={queryClient}>
-            {children}
-            <ReactQueryDevtools initialIsOpen={false} />
-        </TanStackQueryClientProvider>
-    );
+    return <TanStackQueryClientProvider client={queryClient}>{children}</TanStackQueryClientProvider>;
 };
